b6b144fbac06d8465f014b3d898a49a9ff9b6428
MarkCodez <109993306+MarkCodez@users.noreply.github.com>
2024-03-10 22:23:29 -0500

    Controller/transaction type (#7)
    * initial commit for transaction type API. basics done, needs testing and debugging. added necessary scoped and singleton statements for new API in startup file. wip
    
    * ran api tests for transaction types and succesfully called all operations. possible room for more robustness/perks but MVP is good to go.
    
    * NOT ready to be pulled. attempted to add functionality to get method to return all transaction types that are owned by the user. code doesn't work, getting 'TypeError: Window.fetch: HEAD or GET Request cannot have a body.' error message when testing get api method. wip
    
    * Revert "NOT ready to be pulled. attempted to add functionality to get method to return all transaction types that are owned by the user. code doesn't work, getting 'TypeError: Window.fetch: HEAD or GET Request cannot have a body.' error message when testing get api method. wip"
    
    This reverts commit afb8373a0fb980753d8ea3ce1b88a6c2d91d90a9.
    
    * tested transaction type controller. ready to be pushed to main.

---
+++ expenseTrackerAPI/Repositories/TransactionTypeRepository.cs
@@ -0,0 +1,96 @@
+using Dapper;
+using expenseTrackerAPI.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace expenseTrackerAPI.Repositories
+{
+    public class TransactionTypeRepository : ITransactionTypeRepository
+    {
+        private readonly string _connectionString;
+
+        public TransactionTypeRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<TransactionType> GetTransactionTypes()
+        {   
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = $@"SELECT * FROM transactionTypes
+                                WHERE userId IS NULL";
+                return conn.Query<TransactionType>(sql);
+            }
+        }
+
+        public IEnumerable<TransactionType> GetTransactionTypeByUserId(int id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = $@"SELECT * FROM transactionTypes 
+                                WHERE (userId = @id OR userId IS NULL) AND isDeleted = 0";
+                var parameters = new DynamicParameters();
+                parameters.Add("@id", id, DbType.Int16);
+
+                return conn.Query<TransactionType>(sql, parameters);
+            }
+        }
+
+        public int CreateTransactionType(TransactionType transactionType)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                // query for mysql
+                //string sql = $@"INSERT INTO expenses (userId, transactionTypeId, amount, date, description, createdAt, updatedAt, deletedAt, isDeleted)
+                //               VALUES (@userId, @transactionTypeId, @amount, @date, @description, @createdAt, @updatedAt, @deletedAt, @isDeleted); 
+                //               SELECT LAST_INSERT_ID();";
+
+                // query for sql server
+                string sql = $@"INSERT INTO transactionTypes (userId, transactionTypeName, isDeleted)
+                                 VALUES(@userId, @transactionTypeName, @isDeleted);
+                                 SELECT SCOPE_IDENTITY();";
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@userId", transactionType.UserId, DbType.Int16);
+                parameters.Add("@transactionTypeName", transactionType.TransactionTypeName, DbType.String);
+                parameters.Add("@isDeleted", 0, DbType.Boolean);
+
+                return conn.ExecuteScalar<int>(sql, parameters);
+            }
+        }
+
+        public bool UpdateTransactionType(TransactionType transactionType)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = $@"UPDATE transactionTypes 
+                                SET transactionTypeName = @transactionTypeName
+                                WHERE transactionTypeId = @transactionTypeId AND isDeleted = 0;";
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@transactionTypeId", transactionType.TransactionTypeId, DbType.Int16);
+                parameters.Add("@transactionTypeName", transactionType.TransactionTypeName, DbType.String);
+
+                var rows = conn.Execute(sql, parameters);
+                return rows > 0;
+            }
+        }
+
+        public bool DeleteTransactionType(int id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = $@"UPDATE transactionTypes
+                                SET isDeleted = 1
+                                WHERE transactionTypeId = @transactionTypeId";
+                var parameters = new DynamicParameters();
+                parameters.Add("@transactionTypeId", id, DbType.Int16);
+
+                var rows = conn.Execute(sql, parameters);
+                return rows > 0;
+            }
+        }
+    }
+}
