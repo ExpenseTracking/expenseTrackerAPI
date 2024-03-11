4f5b0cbce5ff00c1d74d90cf52abb5bd19142aeb
MarkCodez <109993306+MarkCodez@users.noreply.github.com>
2024-03-10 21:23:37 -0500

    new user roles controller up and running. could use more testing. delete method useless becuase no need for isDeleted field in database. wip (#5)

---
+++ expenseTrackerAPI/Services/UserRolesService.cs
@@ -0,0 +1,40 @@
+﻿using expenseTrackerAPI.Models;
+using expenseTrackerAPI.Repositories;
+
+namespace expenseTrackerAPI.Services
+{
+    public class UserRolesService : IUserRolesService
+    {
+        private readonly IUserRolesRepository _userRolesRepository;
+
+        public UserRolesService(IUserRolesRepository userRolesRepository)
+        {
+            _userRolesRepository = userRolesRepository;
+        }
+
+        public IEnumerable<UserRoles> GetUserRoles()
+        {
+            return _userRolesRepository.GetUserRoles();
+        }
+
+        public UserRoles GetUserRoleById(int id)
+        {
+            return _userRolesRepository.GetUserRoleById(id);
+        }
+
+        public int CreateUserRole(UserRoles userRoles)
+        {
+            return _userRolesRepository.CreateUserRole(userRoles);
+        }
+
+        public bool UpdateUserRole(UserRoles userRoles)
+        {
+            return _userRolesRepository.UpdateUserRole(userRoles);
+        }
+
+        public bool DeleteUserRole(int id)
+        {
+            return _userRolesRepository.DeleteUserRole(id);
+        }
+    }
+}
