bd16f9758887193d3dcbd8a7a37721cbdbe11eb1
MarkCodez <markeskander745@gmail.com>
2024-04-08 14:02:02 -0500

    changed return type to array so that UI can properly read in info.

--- expenseTrackerAPI/Repositories/Interfaces/IUserRepository.cs
+++ expenseTrackerAPI/Repositories/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace expenseTrackerAPI.Repositories
     public interface IUserRepository
     {
         IEnumerable<User> GetUsers();
-        User GetUserById(int id);
+        IEnumerable<User> GetUserById(int id);
         int CreateUser(User user);
         bool UpdateUser(User user);
         bool DeleteUser(int id);
