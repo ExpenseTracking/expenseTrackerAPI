4ecce3bdda89d52d721bf8708f62bb27ab66f698
Andrew Preston <74381686+aprest10@users.noreply.github.com>
2024-04-09 08:46:08 -0500

    add username (#17)
    * add username
    
    * Update TransactionTypeRepository.cs

--- expenseTrackerAPI/Models/TransactionType.cs
+++ expenseTrackerAPI/Models/TransactionType.cs
@@ -11,5 +11,7 @@ namespace expenseTrackerAPI.Models
         public string TransactionTypeName { get; set; }
 
         public bool? IsDeleted { get; set; }
+
+        public string? UserName { get; set; }
     }
 }
