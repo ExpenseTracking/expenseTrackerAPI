4a31fdb957fcfa67d330226728e0b4c2b4a55337
Andrew Preston <74381686+aprest10@users.noreply.github.com>
2024-04-14 16:22:53 -0500

    Update AuthenticationRepository.cs

--- expenseTrackerAPI/Repositories/AuthenticationRepository.cs
+++ expenseTrackerAPI/Repositories/AuthenticationRepository.cs
@@ -15,53 +15,32 @@ namespace expenseTrackerAPI.Repositories
             _connectionString = connectionString;
         }
 
-        // public bool AuthenticateUser(User user)
-        // {
-        //     using (var conn = new SqlConnection(_connectionString))
-        //     {
-        //         string sql = "SELECT count(*) AS match FROM users WHERE username = @username AND password = @password AND isDeleted = 0";
-        //         var parameters = new DynamicParameters();
-        //         parameters.Add("@username", user.Username, DbType.String);
-        //         parameters.Add("@password", user.Password, DbType.String);
-
-        //         int match = (int)conn.ExecuteScalar(sql, parameters);
-
-        //         return match == 1;
-        //     }
-        // }
-
-        // public User GetAuthenticatedUserId(User user) 
-        // {
-        //     using (var conn = new SqlConnection(_connectionString))
-        //     {
-        //         string sql = "SELECT * FROM users WHERE username = @username AND password = @password AND isDeleted = 0";
-        //         var parameters = new DynamicParameters();
-        //         parameters.Add("@username", user.Username, DbType.String);
-        //         parameters.Add("@password", user.Password, DbType.String);
-
-        //         return conn.QuerySingle<User>(sql, parameters);
-        //     }
-        // }
-
-        private readonly List<User> _users = new List<User>
-        {
-            new User { UserId = 1, Username = "user1", Password = "password1", IsDeleted = false },
-            new User { UserId = 2, Username = "user2", Password = "password2", IsDeleted = false }
-            // Add more users as needed
-        };
-
         public bool AuthenticateUser(User user)
         {
-            // Simulate authentication by checking if the username and password match
-            int matchCount = _users.Count(u => u.Username == user.Username && u.Password == user.Password && u.IsDeleted == false);
-            return matchCount == 1;
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT count(*) AS match FROM users WHERE username = @username AND password = @password AND isDeleted = 0";
+                var parameters = new DynamicParameters();
+                parameters.Add("@username", user.Username, DbType.String);
+                parameters.Add("@password", user.Password, DbType.String);
+
+                int match = (int)conn.ExecuteScalar(sql, parameters);
+
+                return match == 1;
+            }
         }
 
-        public User GetAuthenticatedUser(User user)
+        public User GetAuthenticatedUserId(User user) 
         {
-            // Simulate retrieving user details after successful authentication
-            User authenticatedUser = _users.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password && u.IsDeleted == false);
-            return authenticatedUser;
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM users WHERE username = @username AND password = @password AND isDeleted = 0";
+                var parameters = new DynamicParameters();
+                parameters.Add("@username", user.Username, DbType.String);
+                parameters.Add("@password", user.Password, DbType.String);
+
+                return conn.QuerySingle<User>(sql, parameters);
+            }
         }
     }
 }
